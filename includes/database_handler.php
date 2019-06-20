01b451808e53024fdb309fe2c3e6d6add3334e45
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-06-20 21:32:48 +0530

    Added codes for signup.inc.php & database.handler.php

---
+++ includes/database_handler.php
@@ -0,0 +1,14 @@
+<?php
+
+$serverName = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dBName = "login_system";
+
+$connection = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+if(!$connection){
+
+    die("Connection Failed : ".mysqli_connect_error());
+
+}
--- includes/signup_inc.php
+++ includes/signup_inc.php
@@ -1,1 +1,9 @@
 <?php
+
+if(isset($_POST['signup_submit'])){
+
+    require 'database_handler.php';
+
+
+
+}
