25d04eda01bbe70e68bdcd4293407daf79256d25
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-06-21 21:38:24 +0530

    Added codes for all input data

--- includes/signup_inc.php
+++ includes/signup_inc.php
@@ -21,7 +21,7 @@ if(isset($_POST['signup_submit'])){
         header("Location: ../signup.php?error=invalidmail&id_user=".$username);
         exit();
     }
-    else if (!preg_match("/^[a-zA-Z0-9]*$/, $username")){
+    else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)){
         header("Location: ../signup.php?error=invalidid_user&mail=".$email);
         exit();
     }
@@ -31,27 +31,47 @@ if(isset($_POST['signup_submit'])){
     }
     else{
 
+        $sql = "SELECT username FROM users WHERE username=?";
+        $stmt = mysql_stmt_init($connection);
+        if(mysqli_stmt_prepare($stmt, $sql)){
+            header("Location: ../signup.php?error=sqlerror");
+            exit();
+        }
+        else{
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            mysqli_stmt_execute($stmt);
+            mqsqli_stmt_store_result($stmt);
+            $resultCheck = mysqli_stmt_num_rows($stmt);
+            if($resultCheck>0){
+                header("Location: ../signup.php?error=usertaken&mail=".$email);
+                exit();
+            }
+            else{
+                $sql = "INSERT INTO users (username, mail, password) VALUES (?, ?, ?)";
+                $stmt = mysql_stmt_init($connection);
+                if(mysqli_stmt_prepare($stmt, $sql)){
+                    header("Location: ../signup.php?error=sqlerror");
+                    exit();
+                }
+                else{
+                    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+                    mysqli_stmt_bind_param($stmt, "sss", $username,$email, $hashedPassword);
+                    mysqli_stmt_execute($stmt);
+                    header("Location: ../signup.php?signup=success");
+                    exit();
+                    }
+                }
+            }
+        }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($connection);
 
     }
 
-
-
+else{
+    header("Location: ../signup.php");
+    exit();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
