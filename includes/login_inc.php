950d83ef0cccd5e41fa1aea14fefd3abf3e91ef2
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-06-22 23:55:34 +0530

    Added codes for login.inc.php

--- includes/login_inc.php
+++ includes/login_inc.php
@@ -1,2 +1,35 @@
 <?php
+
+if(isset($_POST['login_submit'])){
+
+    require 'database_handler.php';
+
+    $mailId = $_POST['mail'];
+    $password = $_POST['pwd'];
+
+    if(empty($mailId) || empty($password)){
+        header("Location: ../index.php?error=emptyfields");
+        exit();
+    }
+    else{
+        $sql = "SELECT * FROM users WHERE username=? OR mail=?;";
+        $stmt = mysqli_stmt_init($connection);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            header("Location: ../index.php?error=sqlerror");
+            exit();
+        }
+        else{
+            mysqli_stmt_bind_param($stmt, "ss", $mailId, $mailId);
+
+
+        }
+
+    }
+
+}
+else{
+    header("Location: ../index.php");
+    exit();
+}
+
 ?>
